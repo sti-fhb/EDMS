fd56da3c671872f00ac78db89af196045dd7d9fc
KO-TANG <143869487+KO-TANG@users.noreply.github.com>
2026-08-28 10:16:49 +0800

    chore(alembic): merge dp-register-hardening 與 dm-editor-draft 之 migration head

---
+++ backend/alembic/versions/20260827_1608_48e2b53ac893_merge_dp_register_hardening_and_dm_.py
@@ -0,0 +1,26 @@
+"""merge_dp_register_hardening_and_dm_editor_draft
+
+Revision ID: 48e2b53ac893
+Revises: 070865346fb4, dfa09c18c56e
+Create Date: 2026-08-27 16:08:14.755614
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '48e2b53ac893'
+down_revision: Union[str, None] = ('070865346fb4', 'dfa09c18c56e')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
